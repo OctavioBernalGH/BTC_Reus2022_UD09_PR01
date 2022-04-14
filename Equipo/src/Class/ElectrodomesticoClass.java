a7713e8c6517e36a0ab6744dc77f118413a0115c
mednologic <mednologic@gmail.com>
2022-04-14 13:14:45 +0200

    Update ElectrodomesticoClass.java
    "Clase electrodomestico definida, reconsiderando clase abstracta"

--- Equipo/src/Class/ElectrodomesticoClass.java
+++ Equipo/src/Class/ElectrodomesticoClass.java
@@ -2,20 +2,21 @@ package Class;
 
 public class ElectrodomesticoClass {
 	
-	protected final String COLORF	="blanco";
-	protected final char  CONSUMOF	='F';
-	protected double PRECIOBASEF = 100;
-	protected double PESOF = 5;
+	protected static final String COLORF	="blanco";
+	protected static final char  CONSUMOF	='F';
+	protected static double PRECIOBASEF = 100;
+	protected static double PESOF = 5;
 	
 	protected double precioBase = PRECIOBASEF;
 	protected String color = COLORF;
 	protected char consumo = CONSUMOF;
 	protected double peso = PESOF;
+	
+
 
 
 //Constructors
 	public ElectrodomesticoClass() {
-		//Hola
 	}
 
 
@@ -28,9 +29,12 @@ public class ElectrodomesticoClass {
 
 	public ElectrodomesticoClass(double precioBase, String color, char consumo, double peso) {
 		super();
+		//Transform the char to uppercase
+		char charUpper = Character.toUpperCase(consumo);
+		
 		this.precioBase = precioBase;
-		this.color = color;
-		this.consumo = consumo;
+		this.color = comprobarColor(color);
+		this.consumo = comprobarConsumoEnergetico(charUpper);
 		this.peso = peso;
 	}
 	
@@ -38,19 +42,67 @@ public class ElectrodomesticoClass {
 	
 //Methods
 	
-	public static boolean comprobarConsumoEnergetico(char letra) {
-		boolean flag = false;
+	public static char comprobarConsumoEnergetico(char letra) {
+		char letraCorrecta=CONSUMOF;
+		char [] letras = {'A', 'B', 'C', 'D', 'E','F'};
 		
-		return flag;
+		for(int i=0; i<letras.length; i++) {
+			if(letra==letras[i]) {
+				letraCorrecta=letra;
+			}
+		}
+		
+		return letraCorrecta;
 	}
-	public static boolean comprobarColor(String color) {
-		boolean flag = false;
+	public static String comprobarColor(String color) {
+		String colorCorrecto=COLORF;
+		String [] colores = {"blanco", "negro", "rojo", "azul", "gris"};
 		
-		return flag;
+		for(int i=0; i<colores.length;i++) {
+			if(color.toLowerCase().equals(colores[i])) {
+				colorCorrecto = color;
+			}
+		}	
+		return colorCorrecto;
 	}
 
-	public static double precioFinal(char letra) {
-		double precioModificado = 0;
+	public static double precioFinal(char letra, double peso) {
+		double precioModificado = PRECIOBASEF;
+		
+		//Modify price depending the consumption
+		switch(comprobarConsumoEnergetico(letra)) {
+			case 'A':
+				precioModificado = precioModificado + 100;
+				break;
+			case 'B':
+				precioModificado = precioModificado + 80;
+				break;
+			case 'C':
+				precioModificado = precioModificado + 60;
+				break;
+			case 'D':
+				precioModificado = precioModificado + 50;
+				break;
+			case 'E':
+				precioModificado = precioModificado + 30;
+				break;
+			case 'F':
+				precioModificado = precioModificado + 10;
+				break;
+			default:
+					break;
+			}
+	//Modify price depending the weight
+			if(peso>=0 && peso<=19){
+				precioModificado = precioModificado + 10;
+				
+			}else if(peso>=20 && peso<=49) {
+				precioModificado = precioModificado + 50;
+			}else if(peso>=50 && peso<=79) {
+				precioModificado = precioModificado + 80;
+			}else if(peso>=80) {
+				precioModificado = precioModificado + 100;
+			}
 		
 		return precioModificado;
 	}
