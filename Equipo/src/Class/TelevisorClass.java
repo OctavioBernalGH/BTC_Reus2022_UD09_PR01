c6b65fe88e5ce680f325b0a7f7dc38898dfcf4c6
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-04-14 11:45:07 +0200

    Estructura inicial TelevisorClass.java
    Se sube la estructura inicial de la clase.

--- Equipo/src/Class/TelevisorClass.java
+++ Equipo/src/Class/TelevisorClass.java
@@ -1,5 +1,20 @@
 package Class;
 
-public class TelevisorClass {
+public class TelevisorClass extends ElectrodomesticoClass {
 
+	/* Atributos de clase */
+	protected double resolucion;
+	protected boolean sintonizador;
+	
+	/* Constantes por defecto */
+	protected final double RESOLUCIONF = 20.0;
+	protected final boolean SINTONIZADORF = false;
+	
+	/* Constructor por defecto */
+	public TelevisorClass() {
+		this.resolucion = RESOLUCIONF;
+		this.sintonizador = SINTONIZADORF;
+	}
+	
+	
 }
