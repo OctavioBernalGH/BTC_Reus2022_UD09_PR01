806ecfe955ee4db7551e11c4b80c6604183e83a1
mednologic <mednologic@gmail.com>
2022-04-14 09:58:41 +0200

    "Estructura"

---
+++ Equipo/src/Class/ElectrodomesticoClass.java
@@ -0,0 +1,5 @@
+package Class;
+
+public class ElectrodomesticoClass {
+
+}
---
+++ Equipo/src/Class/MainApp.java
@@ -0,0 +1,10 @@
+package Class;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
