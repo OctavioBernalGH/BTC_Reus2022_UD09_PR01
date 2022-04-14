66b6f9de13238a90e496def24acde728f4a9d884
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-04-14 11:45:52 +0200

    Creacion de atributos y constructor

--- Equipo/src/Class/LavadoraClass.java
+++ Equipo/src/Class/LavadoraClass.java
@@ -1,5 +1,15 @@
 package Class;
 
 public class LavadoraClass {
+	// Atributos de clase
+	protected double carga;
+	
+	// Constantes por defecto
+	protected final double CARGAF= 5;
 
+	public LavadoraClass(double carga) {
+		super();
+		this.carga = carga;
+	}	
+	
 }
