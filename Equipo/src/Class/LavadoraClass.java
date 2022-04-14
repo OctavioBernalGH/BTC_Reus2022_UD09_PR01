5fea3531a74e594048e035f50ad96ba98eee8c5d
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-04-14 13:33:38 +0200

    Commit despues de la presentacion

--- Equipo/src/Class/LavadoraClass.java
+++ Equipo/src/Class/LavadoraClass.java
@@ -1,14 +1,44 @@
 package Class;
 
-public class LavadoraClass {
+public class LavadoraClass extends ElectrodomesticoClass{
 	// Atributos de clase
 	protected double carga;
 	
 	// Constantes por defecto
-	protected final double CARGAF= 5;
+	protected static final double CARGAF= 5;
 
 	public LavadoraClass(double carga) {
 		this.carga = carga;
-	}	
+	}
+
+	public LavadoraClass() {
+		this.carga = CARGAF;
+		
+	}
+
+	public LavadoraClass(double precioBase, double peso) {
+		super(precioBase, peso);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LavadoraClass(double precioBase, String color, char consumo, double peso) {
+		super(precioBase, color, consumo, peso);
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	public double getCarga() {
+		return carga;
+	}
+
+	public void setCarga(double carga) {
+		this.carga = carga;
+	}
+
+	@Override
+	public String toString() {
+		return "LavadoraClass [carga=" + carga + "]";
+	}
+	
 	
 }
--- Equipo/src/Class/MainApp.java
+++ Equipo/src/Class/MainApp.java
@@ -4,6 +4,9 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		LavadoraClass lavadora = new LavadoraClass();
+		System.out.println(lavadora);
 
 	}
 
