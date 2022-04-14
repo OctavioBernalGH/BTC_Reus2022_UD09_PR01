17677410a32d55e1e14e79d415b245ba17fde676
mednologic <mednologic@gmail.com>
2022-04-14 10:04:34 +0200

    "prueba"

--- Equipo/src/Class/ElectrodomesticoClass.java
+++ Equipo/src/Class/ElectrodomesticoClass.java
@@ -1,5 +1,15 @@
 package Class;
 
 public class ElectrodomesticoClass {
+	
+	protected double precioBase = 100;
+	protected String color = "blanco";
+	protected char consumo = 'F';
+	protected double peso = 5;
+	
+	
+//Methods
+	
+//Getter and setters
 
 }
